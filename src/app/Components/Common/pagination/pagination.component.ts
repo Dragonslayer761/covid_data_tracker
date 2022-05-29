deeada67a8df9bd5ccaab4d3b4a658b59a3be573
Subham Chowdhur <sc761851@gmail.com>
2022-05-29 17:46:12 +0530

    resuable pagination component added

---
+++ src/app/Components/Common/pagination/pagination.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+  @Input() pageList: number[];
+  @Output() getPage = new EventEmitter();
+  presentPageNumber: any = 1;
+  pageListRender: any;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  getPageNo(event: any) {
+    let val = "";
+    val = event.target.innerText
+    this.presentPageNumber = parseInt(val);
+    this.getPage.emit(this.presentPageNumber);
+  }
+  getPrevious(){
+    if(this.presentPageNumber  > 1){
+      this.presentPageNumber-=1
+    }
+    this.getPage.emit(this.presentPageNumber);
+  }
+  getNext(){
+    if(this.presentPageNumber  < this.pageList.length){
+      this.presentPageNumber+=1
+    }
+    this.getPage.emit(this.presentPageNumber);
+  }
+}
