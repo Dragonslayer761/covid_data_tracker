c87d7aa2e8588939c71d6699460f847e6323788f
Subham Chowdhur <sc761851@gmail.com>
2022-05-29 17:41:55 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CountryData } from './Models/covid.models';
+import { CovidDataTrackerService } from './services/covid-data-tracker.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'covidDataTracker';
+  data:any;
+  countryData : any;
+  constructor(private covidService:CovidDataTrackerService){}
+  ngOnInit(){
+    this.covidService.getAllData();
+    this.covidService.$countryDataEmit.subscribe(data => this.countryData = data)
+  }
+  refreshData(){
+    this.covidService.refreshPage();
+    this.covidService.getAllData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SeacrhBarComponent } from './Components/seacrh-bar/seacrh-bar.component';
+import { DataPreviewComponent } from './Components/data-preview/data-preview.component';
+import { GlobalDataComponent } from './Components/global-data/global-data.component';
+import { CountryDataComponent } from './Components/country-data/country-data.component';
+import { CardComponent } from './Components/Common/card/card.component';
+import { ListComponentComponent } from './Components/Common/list-component/list-component.component';
+import { PaginationComponent } from './Components/Common/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeacrhBarComponent,
+    DataPreviewComponent,
+    GlobalDataComponent,
+    CountryDataComponent,
+    CardComponent,
+    ListComponentComponent,
+    PaginationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas :[CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
