c6d1e829a617bd55f12d34adf2b0e7c53f421feb
Subham Chowdhur <sc761851@gmail.com>
2022-05-29 17:42:39 +0530

    app enums constants

---
+++ src/app/enums/covid.enum.ts
@@ -0,0 +1,13 @@
+export enum sortOrder{
+    asc = "ASC",
+    desc = "DESC"
+}
+export enum ColumnName {
+    country = "Country",
+    totalConfirmed = "Total Confirmed",
+    newConfirmed = "New Confirmed",
+    totalDeath = "Total Death",
+    newDeath = "New Death",
+    totalRecoverd = "Total Recovered",
+    newRecovered = "New Recovered"
+}
